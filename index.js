ba32ca86340e54473a281229458f0549898c214d
pookiepew <glensorbo@gmail.com>
2021-01-11 21:43:24 +0100

    Initial push

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const volleyball = require('volleyball');
+const helmet = require('helmet');
+
+const config = require('./config');
+
+const app = express();
+app.use(cors());
+app.use(volleyball);
+app.use(helmet());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found - ' + req.originalUrl);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(res.statusCode === 200 ? 500 : res.statusCode);
+  res.json({
+    message: error.message,
+    stack: config.NODE_ENV === 'production' ? undefined : error.stack
+  });
+});
+
+const port = config.PORT || 8888;
+
+const server = app.listen(port, () =>
+  console.log('http://localhost:' + server.address().port)
+);
